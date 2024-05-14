20c0a53c21232fe5f902c86f974a0d73ce476d34
Endlin Boeingstein <endlinboeingstein@gmail.com>
2024-05-14 21:57:24 +0800

    新增其余元件帧间空帧检测功能以及所有元件帧间空帧图层自动分离功能

--- Program.cs
+++ Program.cs
@@ -17,14 +17,14 @@ class Program
 		if (args.Length > 0)
 		{
 			FileInfo fileInfo = new FileInfo(args[0]);
-			Console.WriteLine("版本号：1.7.0-public-alpha\t作者：Endlin Boeingstein（滨敔滨纵凝）\n编译时间：2021年12月7日14时32分\t协助调试：小黑_yyds、Min_mozj、ATSGF\n在使用本软件前请提前备份好文件，否则后果自负");
+			Console.WriteLine("版本号：1.8.0-public-alpha\t作者：Endlin Boeingstein（滨敔滨纵凝）\n编译时间：2021年12月7日14时32分\t协助调试：小黑_yyds、Min_mozj、ATSGF\n在使用本软件前请提前备份好文件，否则后果自负");
 			Console.WriteLine("文件路径：" + fileInfo.FullName);
 			filepath = fileInfo.FullName;
 		}
 		//打开程序拖入窗体读取
 		else
 		{
-			Console.WriteLine("版本号：1.7.0-public-alpha\t作者：Endlin Boeingstein（滨敔滨纵凝）\n编译时间：2021年12月7日14时32分\t协助调试：小黑_yyds、Min_mozj、ATSGF\n在使用本软件前请提前备份好文件，否则后果自负");
+			Console.WriteLine("版本号：1.8.0-public-alpha\t作者：Endlin Boeingstein（滨敔滨纵凝）\n编译时间：2021年12月7日14时32分\t协助调试：小黑_yyds、Min_mozj、ATSGF\n在使用本软件前请提前备份好文件，否则后果自负");
 			Console.WriteLine("请将文件拖入窗体，并按回车键");
 			//新功能更新而停用///Console.WriteLine("如果你的文件在C盘（尤其是桌面），那么请退出程序，直接将文件拖放到本应用的图标即可，拖窗体模式无权限修改C盘文件");
 			filepath = Console.ReadLine().Trim('"');
